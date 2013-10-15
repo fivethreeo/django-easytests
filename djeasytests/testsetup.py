3395195eef9d4fef9ae6937736efb44cfae1c5d2
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2013-10-15 14:22:13 +0200

    two fixes

--- djeasytests/testsetup.py
+++ djeasytests/testsetup.py
@@ -185,7 +185,7 @@ Options:
             mapper = pool.map
         else:
             mapper = map
-        results = mapper(_test_in_subprocess, ([test_label, self.path] for test_label in test_labels))
+        results = mapper(_test_in_subprocess, ([(test_label, self.path)] for test_label in test_labels))
         failures = [test_label for test_label, return_code in zip(test_labels, results) if return_code != 0]
         return failures
     
@@ -228,7 +228,7 @@ Options:
     def manage(self):
         self.configure()
         from django.core.management import execute_from_command_line
-        execute_from_command_line([self.filename] + self.args.get('<args>', []))
+        execute_from_command_line(['%s manage' % self.filename] + self.args.get('<args>', []))
                 
     def configure(self, **kwargs):
         migrate = self.args.get('--migrate', False)
